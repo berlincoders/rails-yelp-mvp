5ff8047881f5dc392f65001cd3d6c5689ab36d4c
Romeo <ralonsoad@gmail.com>
2023-08-16 22:01:13 +0200

    yelp-mvm User Journey

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,42 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
-
   def show
+    @review = Review.new
   end
-
   def new
+    @restaurant = Restaurant.new
   end
+  def edit
+  end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: "Restaurant was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: "Restaurant was successfully destroyed.", status: :see_other
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :rating)
+    end
 end
